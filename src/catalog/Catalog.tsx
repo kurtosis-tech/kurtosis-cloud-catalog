fdabdf601c4d1d3c831355aa1997e9118f942858
Ben Gazzard <ben@dartoxia.com>
2024-01-03 14:38:38 +0000

    feat: Update package and catalog pages to match designs (#14)
    * Update package and catalog
    
    * Add nologo file
    
    * Update kurtosis-ui-components

--- src/catalog/Catalog.tsx
+++ src/catalog/Catalog.tsx
@@ -1,13 +1,20 @@
 import { SmallCloseIcon } from "@chakra-ui/icons";
 import {
+  Box,
+  Button,
+  ButtonGroup,
   Flex,
   Heading,
   Icon,
-  IconButton,
   Input,
   InputGroup,
   InputLeftElement,
   InputRightElement,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
 } from "@chakra-ui/react";
 import { GetPackagesResponse, KurtosisPackage } from "kurtosis-cloud-indexer-sdk";
 import {
@@ -16,12 +23,24 @@ import {
   isDefined,
   KurtosisAlert,
   KurtosisPackageCardGrid,
+  KurtosisPackageCardRow,
+  maybeArrayToArray,
   PageTitle,
   useKeyboardAction,
 } from "kurtosis-ui-components";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { BiSortAlt2 } from "react-icons/bi";
 import { FiSearch } from "react-icons/fi";
+import { HiStar } from "react-icons/hi";
+import { IoPlay } from "react-icons/io5";
+import { useSearchParams } from "react-router-dom";
 import { useCatalogContext } from "./CatalogContext";
+
+type SearchState = {
+  term: string;
+  filter: ("saved" | "featured")[]; // TODO: Implement 'featured'
+  sortBy?: "stars" | "runs";
+};
 
 export const Catalog = () => {
   const { catalog } = useCatalogContext();
@@ -43,14 +62,19 @@ type CatalogImplProps = {
 
 const CatalogImpl = ({ catalog }: CatalogImplProps) => {
   const searchRef = useRef<HTMLInputElement>(null);
-  const [searchTerm, setSearchTerm] = useState("");
-  const isSearching = searchTerm.length > 0;
-  const filteredCatalog = useMemo(
-    () => catalog.packages.filter((kurtosisPackage) => kurtosisPackage.name.toLowerCase().indexOf(searchTerm) > -1),
-    [searchTerm, catalog],
-  );
+  const [searchParams, setUrlSearchParams] = useSearchParams();
+  const initialTerm = searchParams.get("t") || "";
+  const initialFilter = maybeArrayToArray(searchParams.get("f")).filter(isDefined);
+  const initialSortBy = searchParams.get("s") || undefined;
 
-  const handlePackageRunClicked = (kurtosisPackage: KurtosisPackage) => {
+  const [searchState, setSearchState] = useState<SearchState>({
+    term: initialTerm,
+    filter: initialFilter as ("saved" | "featured")[],
+    sortBy: initialSortBy as "stars" | "runs",
+  });
+  const isSearching = searchState.term.length > 0 || searchState.filter.length > 0 || isDefined(searchState.sortBy);
+
+  const handlePackageRun = (kurtosisPackage: KurtosisPackage) => {
     window.open(`https://cloud.kurtosis.com/enclave-manager?package-id=${encodeURIComponent(kurtosisPackage.name)}`);
   };
 
@@ -64,13 +88,28 @@ const CatalogImpl = ({ catalog }: CatalogImplProps) => {
         },
         escape: () => {
           if (isDefined(searchRef.current) && searchRef.current === document.activeElement) {
-            setSearchTerm("");
+            setSearchState((searchTerm) => ({ ...searchTerm, term: "" }));
           }
         },
       }),
       [searchRef],
     ),
   );
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.set("t", searchState.term);
+    searchState.filter.forEach((f) => params.set("f", f));
+    if (isDefined(searchState.sortBy)) {
+      params.set("s", searchState.sortBy);
+    }
+    const currentParts = window.location.href.split("?");
+    if (currentParts.length > 1) {
+      setUrlSearchParams(params, { replace: true });
+    } else {
+      setUrlSearchParams(params);
+    }
+  }, [searchState, setUrlSearchParams]);
 
   return (
     <AppPageLayout>
@@ -85,20 +124,21 @@ const CatalogImpl = ({ catalog }: CatalogImplProps) => {
             </InputLeftElement>
             <Input
               ref={searchRef}
-              value={searchTerm}
+              value={searchState.term}
               bgColor={"gray.850"}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={(e) => setSearchState((searchTerm) => ({ ...searchTerm, term: e.target.value }))}
               placeholder={"Search"}
             />
-            <InputRightElement w={"unset"}>
+            <InputRightElement w={"unset"} mr={"8px"}>
               {isSearching ? (
-                <IconButton
-                  aria-label={"Clear search"}
+                <Button
                   variant="ghost"
-                  size={"sm"}
-                  icon={<SmallCloseIcon />}
-                  onClick={() => setSearchTerm("")}
-                />
+                  size={"xs"}
+                  rightIcon={<SmallCloseIcon />}
+                  onClick={() => setSearchState((searchTerm) => ({ filter: [], term: "" }))}
+                >
+                  Clear
+                </Button>
               ) : (
                 <FindCommand whiteSpace={"nowrap"} pr={"10px"} />
               )}
@@ -106,22 +146,144 @@ const CatalogImpl = ({ catalog }: CatalogImplProps) => {
           </InputGroup>
         </Flex>
         {isSearching && (
-          <>
-            <Heading fontSize={"lg"} fontWeight={"medium"}>
-              {filteredCatalog.length} Matches
-            </Heading>
-            <KurtosisPackageCardGrid packages={filteredCatalog} onPackageRunClicked={handlePackageRunClicked} />
-          </>
+          <CatalogSearchResults
+            catalog={catalog}
+            searchState={searchState}
+            onSearchStateChanged={setSearchState}
+            onPackageRunClicked={handlePackageRun}
+          />
         )}
         {!isSearching && (
-          <>
-            <Heading fontSize={"lg"} fontWeight={"medium"}>
-              All
-            </Heading>
-            <KurtosisPackageCardGrid packages={catalog.packages} onPackageRunClicked={handlePackageRunClicked} />
-          </>
+          <CatalogDefaultView
+            catalog={catalog}
+            onSearchStateChanged={setSearchState}
+            onPackageRunClicked={handlePackageRun}
+          />
         )}
       </Flex>
     </AppPageLayout>
+  );
+};
+
+type CatalogSearchResultsProps = {
+  catalog: GetPackagesResponse;
+  searchState: SearchState;
+  onSearchStateChanged: (updater: (oldState: SearchState) => SearchState) => void;
+  onPackageRunClicked: (kurtosisPackage: KurtosisPackage) => void;
+};
+
+const CatalogSearchResults = ({
+  catalog,
+  searchState,
+  onSearchStateChanged,
+  onPackageRunClicked,
+}: CatalogSearchResultsProps) => {
+  const filteredCatalog = useMemo(
+    () =>
+      catalog.packages
+        .filter((kurtosisPackage) => kurtosisPackage.name.toLowerCase().indexOf(searchState.term.toLowerCase()) > -1)
+        .filter((kurtosisPackage) => {
+          if (searchState.filter.length === 0) {
+            return true;
+          }
+          // TODO: Implement 'featured' filtering
+          return false;
+        })
+        .sort((a, b) => {
+          if (searchState.sortBy === "stars") {
+            return a.stars > b.stars ? -1 : a.stars === b.stars ? 0 : 1;
+          }
+          if (searchState.sortBy === "runs") {
+            return b.runCount - a.runCount;
+          }
+          return 0;
+        }),
+    [searchState, catalog],
+  );
+
+  return (
+    <Flex flexDirection={"column"} gap={"32px"} maxW={"1248px"}>
+      <Flex justifyContent={"space-between"} alignItems={"center"}>
+        <Heading fontSize={"lg"} fontWeight={"medium"}>
+          {filteredCatalog.length} Matches
+        </Heading>
+        <ButtonGroup variant={"ghost"} size={"xs"}>
+          <Menu>
+            <MenuButton
+              as={Button}
+              leftIcon={<BiSortAlt2 />}
+              variant={isDefined(searchState.sortBy) ? "activeFilterControl" : "ghost"}
+              colorScheme={isDefined(searchState.sortBy) ? "kurtosisGreen" : "ghost"}
+            >
+              {isDefined(searchState.sortBy) ? `Sorted by ${searchState.sortBy}` : "Sort"}
+            </MenuButton>
+            <MenuList>
+              <MenuItem
+                icon={<HiStar />}
+                onClick={() => onSearchStateChanged((searchState) => ({ ...searchState, sortBy: "stars" }))}
+              >
+                Stars
+              </MenuItem>
+              <MenuItem
+                icon={<IoPlay />}
+                onClick={() => onSearchStateChanged((searchState) => ({ ...searchState, sortBy: "runs" }))}
+              >
+                Run Count
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem onClick={() => onSearchStateChanged((searchState) => ({ ...searchState, sortBy: undefined }))}>
+                Clear
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </ButtonGroup>
+      </Flex>
+      <KurtosisPackageCardGrid packages={filteredCatalog} onPackageRunClicked={onPackageRunClicked} />
+    </Flex>
+  );
+};
+
+type CatalogDefaultViewProps = {
+  catalog: GetPackagesResponse;
+  onSearchStateChanged: (updater: (oldState: SearchState) => SearchState) => void;
+  onPackageRunClicked: (kurtosisPackage: KurtosisPackage) => void;
+};
+
+const CatalogDefaultView = ({ catalog, onPackageRunClicked, onSearchStateChanged }: CatalogDefaultViewProps) => {
+  const mostStarredPackages = useMemo(
+    () => [...catalog.packages].sort((a, b) => (a.stars > b.stars ? -1 : a.stars === b.stars ? 0 : 1)).slice(0, 10),
+    [catalog],
+  );
+  const mostRanPackages = useMemo(
+    () =>
+      [...catalog.packages]
+        .sort((a, b) => (a.runCount > b.runCount ? -1 : a.runCount === b.runCount ? 0 : 1))
+        .slice(0, 10),
+    [catalog],
+  );
+
+  return (
+    <>
+      <Box as={"section"} pb="32px" borderColor={"whiteAlpha.300"} borderBottomWidth={"1px"}>
+        <KurtosisPackageCardRow
+          title={"Most starred"}
+          packages={mostStarredPackages}
+          onPackageRunClicked={onPackageRunClicked}
+          onSeeAllClicked={() => onSearchStateChanged((searchState) => ({ ...searchState, sortBy: "stars" }))}
+        />
+      </Box>
+      <Box as={"section"} pb="32px" borderColor={"whiteAlpha.300"} borderBottomWidth={"1px"}>
+        <KurtosisPackageCardRow
+          title={"Most Ran"}
+          packages={mostRanPackages}
+          onPackageRunClicked={onPackageRunClicked}
+          onSeeAllClicked={() => onSearchStateChanged((searchState) => ({ ...searchState, sortBy: "runs" }))}
+        />
+      </Box>
+      <Heading fontSize={"lg"} fontWeight={"medium"}>
+        All
+      </Heading>
+      <KurtosisPackageCardGrid packages={catalog.packages} onPackageRunClicked={onPackageRunClicked} />
+    </>
   );
 };
--- src/catalog/package/Package.tsx
+++ src/catalog/package/Package.tsx
@@ -1,4 +1,8 @@
-import { Box, Flex, Icon, Image, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react";
+import { Box, Flex, Icon, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react";
+import { DateTime } from "luxon";
+import { IoPlay, IoStar } from "react-icons/io5";
+import { useParams } from "react-router-dom";
+
 import { KurtosisPackage } from "kurtosis-cloud-indexer-sdk";
 import {
   AppPageLayout,
@@ -7,15 +11,13 @@ import {
   isDefined,
   KurtosisAlert,
   KurtosisMarkdown,
+  PackageLogo,
   PackageSourceButton,
   readablePackageName,
   RunKurtosisPackageButton,
   SaveKurtosisPackageButton,
   TitledCard,
 } from "kurtosis-ui-components";
-import { DateTime } from "luxon";
-import { IoStar } from "react-icons/io5";
-import { useParams } from "react-router-dom";
 import { useKurtosisPackage } from "../CatalogContext";
 
 export const Package = () => {
@@ -40,7 +42,7 @@ type PackageImplProps = {
 const PackageImpl = ({ kurtosisPackage }: PackageImplProps) => {
   const runCommand = `kurtosis run ${kurtosisPackage.name}`;
 
-  const handlePackageRunClicked = () => {
+  const handleRunPackage = () => {
     window.open(`https://cloud.kurtosis.com/enclave-manager?package-id=${encodeURIComponent(kurtosisPackage.name)}`);
   };
 
@@ -50,16 +52,22 @@ const PackageImpl = ({ kurtosisPackage }: PackageImplProps) => {
         <PackageHeader kurtosisPackage={kurtosisPackage} />
         <Flex gap={"32px"} h={"100%"} w={"100%"}>
           <Flex gap={"32px"} flexDirection={"column"} maxW={"740px"} w={"100%"}>
-            <TitledCard title={"DESCRIPTION"}>
+            <TitledCard title={"DESCRIPTION"} h={"100%"}>
               <Box p={"0 15px 15px 15px"}>
                 <KurtosisMarkdown>{kurtosisPackage.description}</KurtosisMarkdown>
               </Box>
             </TitledCard>
           </Flex>
           <Flex flexDirection={"column"} gap={"16px"} flex={"1"}>
-            <RunKurtosisPackageButton kurtosisPackage={kurtosisPackage} size={"lg"} onClick={handlePackageRunClicked} />
-            <InputGroup size={"lg"}>
-              <Input value={runCommand} textOverflow={"ellipsis"} />
+            <RunKurtosisPackageButton kurtosisPackage={kurtosisPackage} size={"lg"} onClick={handleRunPackage} />
+            <InputGroup size={"lg"} variant={"solid"}>
+              <Input
+                value={runCommand}
+                textOverflow={"ellipsis"}
+                fontFamily={"Inconsolata"}
+                bgColor={"gray.850"}
+                readOnly
+              />
               <InputRightElement>
                 <CopyButton
                   contentName={"command"}
@@ -78,8 +86,14 @@ const PackageImpl = ({ kurtosisPackage }: PackageImplProps) => {
             >
               View on Github
             </PackageSourceButton>
-            <Flex borderBottomWidth={"1px"} borderBottomColor={"whiteAlpha.300"} gap={"16px"} p={"16px"}>
-              <Flex gap={"16px"} flexDirection={"column"}>
+            <Flex
+              borderBottomWidth={"1px"}
+              borderBottomColor={"whiteAlpha.300"}
+              justifyContent={"space-between"}
+              gap={"32px"}
+              p={"16px"}
+            >
+              <Flex gap={"16px"} flexDirection={"column"} flex={"1"}>
                 <Flex gap={"8px"} color="gray.400" fontWeight={"bold"} alignItems={"center"}>
                   <Icon as={IoStar} w={"12px"} h={"12px"} />
                   <Text as={"span"} textTransform={"uppercase"}>
@@ -88,6 +102,17 @@ const PackageImpl = ({ kurtosisPackage }: PackageImplProps) => {
                 </Flex>
                 <Text as={"span"} fontWeight={"medium"} fontSize={"xl"}>
                   {kurtosisPackage.stars.toString()}
+                </Text>
+              </Flex>
+              <Flex gap={"16px"} flexDirection={"column"} flex={"1"}>
+                <Flex gap={"8px"} color="gray.400" fontWeight={"bold"} alignItems={"center"}>
+                  <Icon as={IoPlay} w={"12px"} h={"12px"} />
+                  <Text as={"span"} textTransform={"uppercase"}>
+                    Run Count
+                  </Text>
+                </Flex>
+                <Text as={"span"} fontWeight={"medium"} fontSize={"xl"}>
+                  {kurtosisPackage.runCount.toString()}
                 </Text>
               </Flex>
             </Flex>
@@ -120,14 +145,7 @@ const PackageImpl = ({ kurtosisPackage }: PackageImplProps) => {
 const PackageHeader = ({ kurtosisPackage }: PackageImplProps) => {
   return (
     <Flex gap={"22px"} w={"100%"}>
-      <Image
-        h={"120px"}
-        w={"120px"}
-        bg={kurtosisPackage.iconUrl !== "" ? "white" : "black"}
-        src={kurtosisPackage.iconUrl || `/logo.png`}
-        fallbackSrc={`/logo.png`}
-        borderRadius={"9px"}
-      />
+      <PackageLogo kurtosisPackage={kurtosisPackage} h={"120px"} w={"120px"} borderRadius={"9px"} />
       <Flex flexDirection={"column"} justifyContent={"space-between"} flex={"1"}>
         <Flex flexDirection={"column"} gap={"8px"}>
           <Text noOfLines={1} fontSize={"xl"}>
@@ -138,7 +156,7 @@ const PackageHeader = ({ kurtosisPackage }: PackageImplProps) => {
           </Text>
         </Flex>
         <Box>
-          <SaveKurtosisPackageButton kurtosisPackage={kurtosisPackage} />
+          <SaveKurtosisPackageButton kurtosisPackage={kurtosisPackage} size={"md"} />
         </Box>
       </Flex>
     </Flex>
